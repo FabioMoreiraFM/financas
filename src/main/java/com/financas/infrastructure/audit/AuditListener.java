e564ac598a0e48d243aaa27fd3ac1facbe4c537f
Fabio Moreira <fabiomoreira.mf@gmail.com>
2022-05-28 15:58:45 -0300

    Ajusta lógica para casos de rotinas automáticas

--- src/main/java/com/financas/infrastructure/audit/AuditListener.java
+++ src/main/java/com/financas/infrastructure/audit/AuditListener.java
@@ -1,7 +1,10 @@
 package com.financas.infrastructure.audit;
 
+import java.util.Optional;
+
 import org.hibernate.envers.RevisionListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 
 import com.financas.infrastructure.security.FinancasSecurity;
 
@@ -10,11 +13,14 @@ public class AuditListener implements RevisionListener{
 	@Autowired
 	private FinancasSecurity financasSecurity;
 	
+	private static String ROTINA_AUTOMATICA = "ROTINA AUTOMÁTICA";
+	
 	@Override
 	public void newRevision(Object revisionEntity) {
 		AuditEntity revEntity = (AuditEntity) revisionEntity;  
 		
-		revEntity.setUsuario(financasSecurity.getAuthentication().getName());
+		Optional<Authentication> auth = Optional.ofNullable(financasSecurity.getAuthentication());
+		revEntity.setUsuario(auth.isPresent() ? auth.get().getName() : ROTINA_AUTOMATICA);
 	}
 	
 }
