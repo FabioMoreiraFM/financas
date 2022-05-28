c7035c2e62bdeffed457622e0487386110b28182
Fabio Moreira <fabiomoreira.mf@gmail.com>
2022-05-28 10:10:01 -0300

    Remove hardcoded

--- src/main/java/com/financas/infrastructure/audit/AuditListener.java
+++ src/main/java/com/financas/infrastructure/audit/AuditListener.java
@@ -1,14 +1,20 @@
 package com.financas.infrastructure.audit;
 
 import org.hibernate.envers.RevisionListener;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.financas.infrastructure.security.FinancasSecurity;
 
 public class AuditListener implements RevisionListener{
 
+	@Autowired
+	private FinancasSecurity financasSecurity;
+	
 	@Override
 	public void newRevision(Object revisionEntity) {
 		AuditEntity revEntity = (AuditEntity) revisionEntity;  
 		
-		revEntity.setUsuario("fabio");
+		revEntity.setUsuario(financasSecurity.getAuthentication().getName());
 	}
 	
 }
